6399ef831eb30d84b40d5e69f6cded6082db92b0
Hamish Jackson-Mee <hamishjacksonmee@yahoo.co.nz>
2014-08-21 11:55:49 +1200

    Connections page Ellipsis

--- assets/src/app/app.js
+++ assets/src/app/app.js
@@ -116,18 +116,19 @@ angular.module( 'symantis', [
 var oCurrentUser = {
 	id: 1, 
 	name: "Hamish Jackson-Mee",
-	handle: "donkeyDickBreath",
+	handle: "The man",
 	at: "@"
 }
 var aUsers = [
 	{
 		id: 1, 
 		name: "Hamish Jackson-Mee",
-		handle: "donkeyDickBreath",
+		handle: "The man",
 		at: "@",
 		tags: ['Designer', 'Developer'],
 		professed: 'Lorem ipsum is a shit placeholder text. Meanwhile I\'m a skilled developer',
-		status: 'online' 
+		status: 'online',
+		bio: 'Polaroid flexitarian pork belly, narwhal Bushwick +1 iPhone selfies Williamsburg butcher. Keffiyeh 8-bit cray Pinterest scenester, Tonx umami. Helvetica literally direct trade, mumblecore slow-carb photo booth blog. Vinyl ugh +1 Odd Future viral, wayfarers bicycle rights Cosby sweater disrupt sustainable Etsy trust fund Kickstarter Pitchfork hashtag. Normcore photo booth chillwave Thundercats, salvia occupy Etsy bitters viral. Truffaut jean shorts plaid Carles Banksy photo booth gastropub viral. Photo booth cliche Tumblr Pitchfork, Neutra skateboard fingerstache quinoa Pinterest small batch retro.'
 	},
 	{
 		id: 2, 
@@ -136,7 +137,8 @@ var aUsers = [
 		at: "@",
 		tags: ['Designer', 'Developer'],
 		professed: 'Lorem ipsum is a shit placeholder text. Meanwhile I\'m a skilled developer',
-		status: 'online' 
+		status: 'online',
+		bio: 'Polaroid flexitarian pork belly, narwhal Bushwick +1 iPhone selfies Williamsburg butcher. Keffiyeh 8-bit cray Pinterest scenester, Tonx umami. Helvetica literally direct trade, mumblecore slow-carb photo booth blog. Vinyl ugh +1 Odd Future viral, wayfarers bicycle rights Cosby sweater disrupt sustainable Etsy trust fund Kickstarter Pitchfork hashtag. Normcore photo booth chillwave Thundercats, salvia occupy Etsy bitters viral. Truffaut jean shorts plaid Carles Banksy photo booth gastropub viral. Photo booth cliche Tumblr Pitchfork, Neutra skateboard fingerstache quinoa Pinterest small batch retro.'
 	},
 	{
 		id: 3, 
@@ -145,7 +147,8 @@ var aUsers = [
 		at: "@",
 		tags: ['Designer', 'Developer'],
 		professed: 'Lorem ipsum is a shit placeholder text. Meanwhile I\'m a skilled developer',
-		status: 'offline' 
+		status: 'offline',
+		bio: 'Polaroid flexitarian pork belly, narwhal Bushwick +1 iPhone selfies Williamsburg butcher. Keffiyeh 8-bit cray Pinterest scenester, Tonx umami. Helvetica literally direct trade, mumblecore slow-carb photo booth blog. Vinyl ugh +1 Odd Future viral, wayfarers bicycle rights Cosby sweater disrupt sustainable Etsy trust fund Kickstarter Pitchfork hashtag. Normcore photo booth chillwave Thundercats, salvia occupy Etsy bitters viral. Truffaut jean shorts plaid Carles Banksy photo booth gastropub viral. Photo booth cliche Tumblr Pitchfork, Neutra skateboard fingerstache quinoa Pinterest small batch retro.'
 	},
 	{
 		id: 4, 
@@ -154,7 +157,8 @@ var aUsers = [
 		at: "@",
 		tags: ['Designer', 'Developer'],
 		professed: 'Lorem ipsum is a shit placeholder text. Meanwhile I\'m a skilled developer',
-		status: 'offline' 
+		status: 'offline',
+		bio: 'Polaroid flexitarian pork belly, narwhal Bushwick +1 iPhone selfies Williamsburg butcher. Keffiyeh 8-bit cray Pinterest scenester, Tonx umami. Helvetica literally direct trade, mumblecore slow-carb photo booth blog. Vinyl ugh +1 Odd Future viral, wayfarers bicycle rights Cosby sweater disrupt sustainable Etsy trust fund Kickstarter Pitchfork hashtag. Normcore photo booth chillwave Thundercats, salvia occupy Etsy bitters viral. Truffaut jean shorts plaid Carles Banksy photo booth gastropub viral. Photo booth cliche Tumblr Pitchfork, Neutra skateboard fingerstache quinoa Pinterest small batch retro.'
 	},
 	{
 		id: 4, 
@@ -163,7 +167,8 @@ var aUsers = [
 		at: "@",
 		tags: ['Designer', 'Developer'],
 		professed: 'Lorem ipsum is a shit placeholder text. Meanwhile I\'m a skilled developer',
-		status: 'offline' 
+		status: 'offline',
+		bio: 'Polaroid flexitarian pork belly, narwhal Bushwick +1 iPhone selfies Williamsburg butcher. Keffiyeh 8-bit cray Pinterest scenester, Tonx umami. Helvetica literally direct trade, mumblecore slow-carb photo booth blog. Vinyl ugh +1 Odd Future viral, wayfarers bicycle rights Cosby sweater disrupt sustainable Etsy trust fund Kickstarter Pitchfork hashtag. Normcore photo booth chillwave Thundercats, salvia occupy Etsy bitters viral. Truffaut jean shorts plaid Carles Banksy photo booth gastropub viral. Photo booth cliche Tumblr Pitchfork, Neutra skateboard fingerstache quinoa Pinterest small batch retro.'
 	}
 ]
 
