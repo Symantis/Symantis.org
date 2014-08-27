4c40cb67b0cddb89a1825a382c533b1e7cc0c4b5
Hamish Jackson-Mee <hamishjacksonmee@yahoo.co.nz>
2014-08-27 18:07:32 +1200

    Menu Update and About page

--- assets/src/app/sitenav/index.js
+++ assets/src/app/sitenav/index.js
@@ -19,14 +19,12 @@ angular.module( 'symantis.sitenav', [
 
     var siteItems = [
         {title: 'Home', translationKey: 'navigation:docs', url:'/home'},
-        {title: 'Getting Started', translationKey: 'navigation:start', url:'/start'},
+        {title: 'Get Started', translationKey: 'navigation:start', url:'/start'},
         {title: 'Documentation', translationKey: 'navigation:docs', url:'/docs'},
         {title: 'Community', translationKey: 'navigation:community', url:'/community'},
-        {title: 'Creator App', translationKey: 'navigation:creator', url:'/creator'}
-        
+        {title: 'Creator App', translationKey: 'navigation:creator', url:'/creator'},
+        {title: 'About', translationKey: 'navigation:about', url:'/about'}
     ];
-
-    
 
     $scope.siteItems = siteItems;
 });
