af7954c5810d72a16f12ec400744e89ebbfce4dc
Hamish Jackson-Mee <hamishjacksonmee@yahoo.co.nz>
2014-09-22 10:45:08 +1200

    Add a tag Update

--- assets/bower_components/ng-tags-input/ng-tags-input.js
+++ assets/bower_components/ng-tags-input/ng-tags-input.js
@@ -218,7 +218,7 @@ tagsInput.directive('tagsInput', ["$timeout","$document","tagsInputConfig", func
 
             tagsInputConfig.load('tagsInput', $scope, $attrs, {
                 type: [String, 'text', validateType],
-                placeholder: [String, 'Add a tag'],
+                placeholder: [String, 'Add a tag (Seperate with a coma)'],
                 tabindex: [Number, null],
                 removeTagSymbol: [String, String.fromCharCode(215)],
                 replaceSpacesWithDashes: [Boolean, true],
