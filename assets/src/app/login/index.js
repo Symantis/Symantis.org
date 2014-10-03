d798caee8ce0598a22ec78416b224383b9a0543d
Scott Wyatt <scottwyatt86@gmail.com>
2014-10-03 15:28:48 +1300

    Add Forgotten Password State

--- assets/src/app/login/index.js
+++ assets/src/app/login/index.js
@@ -3,31 +3,45 @@ angular.module( 'symantis.login', [
 
 .config(function config( $stateProvider, $urlRouterProvider) {
 
-	$stateProvider.state( 'login', {
-		url: '/login',
-		views: {
-			"main": {
-				controller: 'LoginCtrl',
-				templateUrl: 'login/index.tpl.html'
-			},
-			"sitenav": {
-				controller: 'SiteNavCtrl',
-                templateUrl: 'sitenav/index.tpl.html'
-			},
-			"header": {
-				controller: 'HeaderCtrl',
-                template: ''
-			},
-			"subheader@login": {
-				controller: 'LoginHeaderCtrl',
-                templateUrl: 'login/header.tpl.html'
+	$stateProvider
+		.state( 'login', {
+			url: '/login',
+			views: {
+				"main": {
+					controller: 'LoginCtrl',
+					templateUrl: 'login/index.tpl.html'
+				},
+				"sitenav": {
+					controller: 'SiteNavCtrl',
+	                templateUrl: 'sitenav/index.tpl.html'
+				},
+				"header": {
+					controller: 'HeaderCtrl',
+	                template: ''
+				},
+				"subheader@login": {
+					controller: 'LoginHeaderCtrl',
+	                templateUrl: 'login/header.tpl.html'
+				}
 			}
-		}
-	});
+		})
+		.state( 'login.forgot', {
+			url: '/forgot',
+			views: {
+				"main@": {
+					controller: 'LoginForgotCtrl',
+					templateUrl: 'login/forgot.tpl.html'
+				}
+			}
+		})
+	;
 })
 
 .controller( 'LoginCtrl', function LoginController( $scope, titleService ) {
 	titleService.setTitle('Login');
+})
+.controller( 'LoginForgotCtrl', function LoginForgotController( $scope, titleService ) {
+	titleService.setTitle('Forgot Password');
 })
 .controller( 'LoginHeaderCtrl', function LoginHeaderController( $scope ) {
 
