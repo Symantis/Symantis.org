8bd5d8488eb889b68689dedc8aeaa9e7393eca77
Hamish Jackson-Mee <hamishjacksonmee@yahoo.co.nz>
2014-10-09 17:58:16 +1300

    Profile Information Update

--- assets/src/app/profile/information/index.js
+++ assets/src/app/profile/information/index.js
@@ -1,25 +1,12 @@
 angular.module( 'symantis.profile.information', [
 ])
 
-
-.controller( 'ProfileInformationCtrl', function ProfileInformationController($http, $scope, user, titleService, $state, $stateParams, UserModel, cache, utils) {
+.controller( 'ProfileInformationCtrl', function ProfileInformationController($scope, titleService, $state, $stateParams) {
 	
-	$scope.user = user;
-	$scope.user.reciprocal = utils.findUserMatches($scope.user.toConnections, $scope.user.fromConnections ).length;
-	//$scope.user.totalToConnections = $scope.user.totalToConnections - $scope.user.reciprocal;
-	//$scope.user.totalFromConnections = $scope.user.totalFromConnections - $scope.user.reciprocal;
-	/*
-	$scope.user = UserModel.getOneHandle(user.handle).then(function(model){
-		return model;
-	});
-	*/
-	//cache.resolveUserCache($scope.users, user.handle);
-
 	titleService.setTitle('Information');
 
-	
-
 })
+
 .controller( 'ProfileInformationLeftsideCtrl', function ProfileActivityLeftsideController( $scope ) {
 	
-});
+})
