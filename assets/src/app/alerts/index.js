5fc51b92b56b86399c701429c8b085dd407a5393
Scott Wyatt <scottwyatt86@gmail.com>
2014-10-12 11:17:51 +1300

    Add alerts as Include.
    Requires Lift.

---
+++ assets/src/app/alerts/index.js
@@ -0,0 +1,20 @@
+angular.module( 'symantis.alerts', [
+])
+
+// .config(function config( $stateProvider, $urlRouterProvider) {
+
+//     $stateProvider.state( '*', {
+//         url: '/*',
+//         views: {
+//             "footer": {
+//                 controller: 'FooterCtrl',
+//                 templateUrl: 'footer/index.tpl.html'
+//             }
+//         }
+//     });
+// })
+
+.controller( 'AlertsCtrl', function AlertsController( $scope, $state, config ) {
+    //$scope.currentUser = config.currentUser;
+
+});
--- assets/src/app/app.js
+++ assets/src/app/app.js
@@ -31,6 +31,7 @@ angular.module( 'symantis', [
 	'symantis.sitenav',
 	'symantis.home',
 	'symantis.footer',
+	'symantis.alerts',
 
 	'symantis.messages',
 	'symantis.kitchen',
