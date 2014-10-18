449cd8103dab5b0dc01fe5220fb082ee871f9426
Scott Wyatt <scottwyatt86@gmail.com>
2014-10-18 19:12:00 +1300

    Starting production build

--- tasks/register/compileAssets.js
+++ tasks/register/compileAssets.js
@@ -2,7 +2,7 @@ module.exports = function (grunt) {
 	grunt.registerTask('compileAssets', [
 		'clean:dev',
 		'html2js:dev',
-		'less:dev',
+		//'less:dev',
 		'compass:dist',
 		'copy:dev',
 		'coffee:dev'
